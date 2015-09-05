ee1ff4c3e86b2bee930adbca2cba80936fcaf3e7
ken macleod <kenmacleod@Kens-MacBook-Pro.local>
2015-09-05 15:18:10 +1000

    init

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,38 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+ require 'scraperwiki'
+ require 'mechanize'
+
 #
-# agent = Mechanize.new
+ agent = Mechanize.new
 #
-# # Read in a page
-# page = agent.get("http://foo.com")
+ page = agent.get("http://www.rfs.nsw.gov.au/fire-information/fdr-and-tobans")
+
+ page.at('tbody').search('tr').each do |row| 
+   name = row.search(:td).first.inner_text
+   dangerToday = row.search('td')[1].inner_text
+   toBanToday = row.search(:td)[2].inner_text
+   fireDangerTomorrow = row.search(:td)[3].inner_text
+   toBanTomorrow = row.search(:td)[4].inner_text
+   councils = row.search(:td)[5].inner_text
+
+  record = {
+    date_scraped: Date.today,
+    name: name,
+    dangerToday: dangerToday,
+    toBanToday: toBanToday,
+    fireDangerTomorrow: fireDangerTomorrow,
+    toBanTomorrow: toBanTomorrow,
+    councils: councils
+  }
+
+  p record
+  ScraperWiki.save_sqlite([:name], record)
+end
+
+puts 'done'
+
 #
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
